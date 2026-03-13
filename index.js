8ddfd13a9cb66ac1980e7aada49a99945f0b64df
Bharadwaj <rbharadwaj022@gmail.com>
2026-03-13 17:23:24 +0530

    CORS new resolution

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ dbconnection();
 //Redeploying
 //Middle Wear
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.options("*", cors());
 
 //Routes
